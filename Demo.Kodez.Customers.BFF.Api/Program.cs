2a8deffe191483347b36a3766640e60614e6ac11
Cheranga Hatangala <cheranga@gmail.com>
2022-03-14 10:00:16 +1100

    adding multiple Azure app configuration providers.

--- Demo.Kodez.Customers.BFF.Api/Program.cs
+++ Demo.Kodez.Customers.BFF.Api/Program.cs
@@ -6,6 +6,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Transactions;
+using Azure.Identity;
 
 namespace Demo.Kodez.Customers.BFF.Api
 {
@@ -20,6 +22,50 @@ namespace Demo.Kodez.Customers.BFF.Api
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+
+                    webBuilder.ConfigureAppConfiguration(builder =>
+                    {
+                        var config = builder.Build();
+                        var appConfigString = config["AzureAppConfigurationUrl"];
+                        var customersBffConfigUrl = config["CustomersBFFAzureAppConfigurationUrl"];
+
+                        builder.AddAzureAppConfiguration(options =>
+                        {
+                            var credentials = new DefaultAzureCredential();
+
+                            options.Connect(new Uri(appConfigString), credentials)
+                                .ConfigureKeyVault(vaultOptions =>
+                                {
+                                    vaultOptions.SetCredential(credentials);
+                                })
+                                .ConfigureRefresh(refreshOptions =>
+                                {
+                                    refreshOptions.Register("RefreshAll", true).SetCacheExpiration(TimeSpan.FromSeconds(5));
+                                })
+                                .UseFeatureFlags(flagOptions =>
+                                {
+                                    flagOptions.CacheExpirationInterval = TimeSpan.FromSeconds(5);
+                                });
+                        }).AddAzureAppConfiguration(options =>
+                        {
+                            var credentials = new DefaultAzureCredential();
+
+                            options.Connect(new Uri(customersBffConfigUrl), credentials)
+                                .ConfigureKeyVault(vaultOptions =>
+                                {
+                                    vaultOptions.SetCredential(credentials);
+                                })
+                                .ConfigureRefresh(refreshOptions =>
+                                {
+                                    refreshOptions.Register("RefreshAll", true).SetCacheExpiration(TimeSpan.FromSeconds(5));
+                                })
+                                .UseFeatureFlags(flagOptions =>
+                                {
+                                    flagOptions.CacheExpirationInterval = TimeSpan.FromSeconds(5);
+                                });
+                        }); ;
+                    });
+
                     webBuilder.UseStartup<Startup>();
                 });
     }
--- Demo.Kodez.Customers.BFF.Api/Startup.cs
+++ Demo.Kodez.Customers.BFF.Api/Startup.cs
@@ -1,19 +1,4 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
-using Demo.Kodez.Customers.BFF.Api.Features.CreateCustomer;
 using Demo.Kodez.Customers.BFF.Api.Features.CreateCustomer.Models;
 using Demo.Kodez.Customers.BFF.Api.Features.CreateCustomer.Services;
 using Demo.Kodez.Customers.BFF.Api.Features.UpdateCustomer.Models;
@@ -21,8 +6,14 @@ using Demo.Kodez.Customers.BFF.Api.Features.UpdateCustomer.Services;
 using Demo.Kodez.Customers.BFF.Api.Shared;
 using Demo.Kodez.Customers.BFF.Api.Shared.Services;
 using FluentValidation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 using Microsoft.FeatureManagement;
+using Microsoft.OpenApi.Models;
 
 namespace Demo.Kodez.Customers.BFF.Api
 {
@@ -38,31 +29,33 @@ namespace Demo.Kodez.Customers.BFF.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddFeatureManagement();
 
-            services.AddFeatureManagement();
+            services.AddAzureAppConfiguration()
+                .AddFeatureManagement();
 
             RegisterConfigurations(services, Configuration);
             RegisterServices(services);
             RegisterValidators(services);
             RegisterResponseBuilders(services);
 
-            
-            
+
             services.AddControllers().AddJsonOptions(options =>
             {
                 options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
             });
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo.Kodez.Customers.BFF.Api", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "Demo.Kodez.Customers.BFF.Api", Version = "v1"});
             });
         }
 
         private void RegisterConfigurations(IServiceCollection services, IConfiguration configuration)
         {
-            services.Configure<CustomerIdentityServiceConfig>(configuration.GetSection(nameof(CustomerIdentityServiceConfig)));
-            services.Configure<CustomerProfileServiceConfig>(configuration.GetSection(nameof(CustomerProfileServiceConfig)));
-            
+            services.Configure<CustomerIdentityServiceConfig>(
+                configuration.GetSection("CustomersBFF:IdentityApiConfig"));
+            services.Configure<CustomerProfileServiceConfig>(configuration.GetSection("CustomersBFF:ProfileApiConfig"));
+
             services.AddScoped(provider =>
             {
                 var config = provider.GetRequiredService<IOptionsSnapshot<CustomerIdentityServiceConfig>>().Value;
@@ -107,8 +100,9 @@ namespace Demo.Kodez.Customers.BFF.Api
                 {
                     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Demo.Kodez.Customers.BFF.Api v1");
                 });
-                
             }
+
+            app.UseAzureAppConfiguration();
 
             app.UseHttpsRedirection();
 
@@ -116,10 +110,7 @@ namespace Demo.Kodez.Customers.BFF.Api
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
